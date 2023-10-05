fc28af2250d68f8ce9f17754942b1be22a7efc84
Mohamed Iyadh Khan <129236582+Iyadh27@users.noreply.github.com>
2023-10-05 10:05:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app = express();
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html");
 })
+app.post("/", )
 
 app.listen(3000, ()=>{
     console.log("server is running at port 3000!");
